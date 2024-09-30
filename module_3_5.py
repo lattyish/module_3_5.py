8d28017dcbf7822d7dc0b3a7ec873d8870ad270f
lattyish <157064983+lattyish@users.noreply.github.com>
2024-10-01 00:09:35 +0500

    Create module_3_5.py

---
+++ module_3_5.py
@@ -0,0 +1,12 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    if len(str_number) <= 1:
+        return int(str_number)
+
+    first = int(str_number[0])
+    remaining_number = int(str_number[1:])
+    if remaining_number == 0:
+        return first
+    return first * get_multiplied_digits(remaining_number)
+result = get_multiplied_digits(304000)
+print(result)
